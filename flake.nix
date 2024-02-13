d206217d5125d02b4981bdee6e41f36600fdaa04
Matthew Doherty <mdoherty@anynines.com>
2024-02-13 21:39:44 +0100

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -5,18 +5,15 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages."x86_64-darwin".default = let
-	      pkgs = nixpkgs.legacyPackages."x86_64-darwin";
-	    in pkgs.buildEnv {
-	      name = "home-packages";
-	      paths = with pkgs; [
-	
-	        # general tools
-	        git
-	
-			# ... add your tools here
-	      ];
-	    };
-	  };
-	
+  packages."x86_64-darwin".default = let
+      pkgs = nixpkgs.legacyPackages."x86_64-darwin";
+	in pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+
+        # general tools
+        git
+      ];
+    };
+  };
 }
